2e8dfb0e1b2df4e4f4617014a02b8daae8bf0476
Aman Pal <aman957551@gmail.com>
2026-08-06 11:02:33 +0530

    Add files via upload

---
+++ password_genrate.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+length = int(input("Password length: "))
+chars = (
+    string.ascii_letters+string.digits+string.punctuation
+)
+password = "".join(
+    random.choice(chars)
+    for _ in range(length)
+)
+print("\nGenrated Password:")
+print(password)
