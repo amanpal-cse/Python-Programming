dbc0c46f84edc47e1defb588655525ebe6a42102
Aman Pal <aman957551@gmail.com>
2026-08-31 21:33:01 +0530

    Add files via upload

---
+++ lamda_function.py
@@ -0,0 +1,12 @@
+square = lambda x: x * x
+
+print(square(5))
+
+add = lambda a, b: a + b
+
+print(add(10, 20))
+
+check = lambda n: "Even" if n % 2 == 0 else "Odd"
+
+print(check(10))
+print(check(7))
---
+++ map_funtion.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+
+result = map(lambda x: x * x, numbers)
+
+print(list(result))
+result = map(lambda x: x * 2, numbers)
+
+print(list(result))
