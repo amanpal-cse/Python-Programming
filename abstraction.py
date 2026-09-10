072f405a8e03160638b6656b86feb9a245e0f90b
Aman Pal <aman957551@gmail.com>
2026-09-10 21:37:25 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Car starts with a key")
+
+
+class Bike(Vehicle):
+
+    def start(self):
+        print("Bike starts with a button")
+
+
+c = Car()
+b = Bike()
+
+c.start()
+b.start()
